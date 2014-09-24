6ed71292b6c35de15345db8c90741820418de8b9
Alice Xue <alicex123@gmail.com>
2014-09-23 21:21:55 -0400

    codingbat

---
+++ 05-codingbat/Driver.java
@@ -0,0 +1,16 @@
+public class Driver {
+    public static void main(String[] args){
+	StringStuff a;
+	a=new StringStuff();
+	System.out.println(a.nonStart("Hello","There"));
+
+	StringStuff b;
+	b=new StringStuff();
+	System.out.println(b.makeAbba("Hi","Bye"));
+
+	StringStuff c;
+	c=new StringStuff();
+	System.out.println(c.diff21(19));
+
+    }
+}
