c61510fb65495019aab1b9e8f35420b57ace12db
Alice Xue <alicex123@gmail.com>
2014-10-02 21:52:20 -0400

    added hw from 10/2/14

--- 07-shapes/Driver.java
+++ 07-shapes/Driver.java
@@ -3,7 +3,10 @@ public class Driver {
 	Shapes a = new Shapes();
 	System.out.println(a.box(4,5));
 	System.out.println(a.tri1(4));
+	System.out.println(a.tri2(4));
 	System.out.println(a.tri3(4));
+	System.out.println(a.diamond(5));
+	System.out.println(a.tri4(4));
 
     }
 }
--- 07-shapes/Shapes.java
+++ 07-shapes/Shapes.java
@@ -1,9 +1,13 @@
+//all working. while and for loops included for all
+
 public class Shapes {
 
     public String box(int r, int c){
 	String s= "";
-	int row = 0;
+	int row;
 	int col;
+	/*
+	row = 0;
 	while (row<r){
 	    col = c;
 	    while (col>0){
@@ -13,14 +17,23 @@ public class Shapes {
 	    s=s+"\n";
 	    row=row+1;
 	}
+	*/
+	for (row=0;row<r;row++) {
+	    for (col=c;col>0;col--) {
+		s = s + "*";
+	    }
+	    s = s + "\n";
+	}
 	return s;
     }
 
     public String tri1(int h) {
 	String s = "";
 	int height = 0;
+	int star;
+	/*
         while (height < h) {
-	    int star = height + 1;
+	    star = height + 1;
 	    while (star > 0) {
 		s = s + "*";
 		star = star - 1;
@@ -28,6 +41,13 @@ public class Shapes {
 	    s = s + "\n";
 	    height = height + 1;
       	}
+	*/
+	for (height=0;height<h;height++) {
+	    for (star=height+1;star>0;star--) {
+		s = s + "*";
+	    }
+	    s = s + "\n";
+	}
 	return s;
     }
 
@@ -35,9 +55,12 @@ public class Shapes {
 	String s = "";
 	int star = 1;
 	int height = h;
+	int row;
+	int space;
+	/*
 	while (height > 0) {
-	    int row = star;
-	    int space = h - star;
+	    row = star;
+	    space = h - star;
 	    while (space > 0) {
 		s = s + " ";
 		space = space -1;
@@ -50,27 +73,152 @@ public class Shapes {
 	    star = row + 1;
 	    height = height - 1;
 	}
+	*/
+	for (height=h;height>0;height--) {
+	    row = star;
+	    for (space=h-star;space>0;space--) {
+		s = s + " ";
+	    }
+	    for (space=h-star;star>0;star--) {
+		s = s + "*";
+	    }
+	    s = s + "\n";
+	    star = row + 1;
+	}
 	return s;
     }
 
     public String tri3(int h) {
 	int row = 0;
-	String s = "";
-	int c = 0; 
-	int r = 0;
+	int row2 = 1;
+	String s = ""; 
+	int spaces = 0;
+	int stars;
+	/*
 	while (row<h) {
-	    r = h - row - 1;
-	    while (r>0) {
+	    spaces = h - row - 1;
+	    while (spaces>0) {
 		s = s + " ";
-		r = r -1;
+		spaces = spaces - 1;
 	    }
-	    c = 2*row + 1;
-	    while (c<=2*h-1) {
+	    stars = 2 * (row2 - 1) + 1;
+	    while (stars > 0) {
 		s = s + "*";
-		c = c - 1;
+		stars = stars -1;
 	    }
 	    row = row + 1;
+	    row2 = row2 + 1;
+	    s = s + "\n";
+	}
+	*/
+	for (row=0;row<h;row++) {
+	    for (spaces=h-row-1;spaces>0;spaces--) {
+		s = s + " ";
+	    }
+	    for (stars=2*(row2-1)+1;stars>0;stars--) {
+		s = s + "*";
+	    }
+	    row2 = row2 + 1;
+	    s = s + "\n";
 	}
 	return s;
     }
+
+    public String diamond (int h) {
+	String s = "";
+	int row = 1;
+	int height = h;
+	int space;
+	int stars;
+	/*
+	while (row<=h) {
+	    if (row <= h/2) {
+		space = (height-1)/2;
+	    } else if (row == h/2 + 1) {
+		space = 0;
+	    } else {
+		space = (row-1)/2;
+	    }
+	    while (space>0) {
+		s = s + " ";
+		space = space - 1;
+	    }
+	    if (row <= h/2) {
+		stars = (2 * (row - 1) + 1);
+		while (stars > 0) {
+		    s = s + "*";
+		    stars = stars - 1;
+		}
+	    } else {
+		stars = (2 * Math.abs(row - h) + 1);
+		while (stars > 0) {
+		    s = s + "*";
+		    stars = stars - 1;
+		}
+	    }
+	    height = height - 1;
+	    row = row + 1;
+	    s = s + "\n";
+	}
+	*/
+	for (row=1;row<=h;row++) {
+	    if (row <= h/2) {
+		space = (height-1)/2;
+	    } else if (row == h/2 + 1) {
+		space = 0;
+	    } else {
+		space = (row-1)/2;
+	    }
+	    for (space=space;space>0;space--) {
+		s = s + " ";
+	    }
+	    if (row <= h/2) {
+		for (stars=2*(row-1)+1;stars>0;stars--) {
+		    s = s + "*";
+		}
+	    } else {
+		for (stars=2*Math.abs(row-h)+1;stars>0;stars--) {
+		    s = s + "*";
+		}
+	    }
+	    height = height - 1;
+	    s = s + "\n";
+	}
+	return s;
+    }
+
+    public String tri4(int h) {
+	String s = "";
+	int height = 0;
+	int space;
+	int star;
+	/*
+        while (height <= h) {
+	    space = height + 1;
+	    while (space > 0) {
+		s = s + " ";
+		space = space - 1;
+	    }
+	    star = h - height- 1;
+	    while (star >= 0) {
+		s = s + "*";
+		star = star - 1;
+	    }
+	    s = s + "\n";
+	    height = height + 1;
+      	}
+	*/
+	for (height=0;height<=h;height++) {
+	    for (space=height+1;space>0;space--) {
+		s = s + " ";
+	    }
+	    for (star=h-height-1;star>=0;star--) {
+		s = s + "*";
+	    }
+	    s = s + "\n";
+	}
+	return s;
+    }
+
+	
 }
