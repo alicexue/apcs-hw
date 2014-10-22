1f8f4dde940fcfdc9e1adec6c48d12c0e0d60320
Alice Xue <alicex123@gmail.com>
2014-10-22 08:08:54 -0400

    uploaded project

--- classwork/inheritance/Basechar.java
+++ classwork/inheritance/Basechar.java
@@ -1,7 +1,13 @@
 /* superclass of Warrior and Mage*/
 public class Basechar {
     private int health=20;
-    private int manna=0;
+    // private int manna=0;
+    private String name;
+
+    public void attack(Basechar other){
+	//System.out.println(this.getName() + " is attacking " + other.getName());
+	System.out.println(this+" is attacking "+other);
+    }
     
     public int getHealth() {
 	return health;
