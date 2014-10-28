d6fe59337c39f1b9f69f4ae8def5e77d5ad5959a
Alice Xue <alicex123@gmail.com>
2014-10-27 21:00:23 -0400

    added hw from 10/27

---
+++ 14-arraystuff/Driver.java
@@ -0,0 +1,8 @@
+public class Driver {
+    public static void main(String[] args) {
+	arrays a = new arrays();
+	System.out.println(a);
+	System.out.println(a.find(100));
+	System.out.println(a.maxVal());
+    }
+}
