aceb5ce075621270e1c2c917bf0f5ea14948acfb
Alice Xue <alicex123@gmail.com>
2014-10-28 22:45:09 -0400

    added hw 14b

--- 14-arraystuff/Driver.java
+++ 14-arraystuff/Driver.java
@@ -4,5 +4,19 @@ public class Driver {
 	System.out.println(a);
 	System.out.println(a.find(100));
 	System.out.println(a.maxVal());
+	System.out.println(a.freq(1));
+	
+	codingbat b = new codingbat();
+	int[] c = {1,2,2};
+	System.out.println(b.sum67(c));
+	int[] d = {1,4,1};
+	System.out.println(b.more14(d));
+	int[] e = {2,10,3,4,20,5};
+	System.out.println(b.tenRun(e));
+	int[] f = {1,4,5,6,2};
+	System.out.println(b.tripleUp(f));
+	int[] g = {1,1,1,2,1};
+	System.out.println(b.canBalance(g));
+	System.out.println(b.seriesUp(3));
     }
 }
---
+++ 14-arraystuff/codingbat.java
@@ -0,0 +1,101 @@
+public class codingbat {
+       public int sum67(int[] nums) {
+	   int sum=0;
+	   boolean prev6 = false;
+	   for (int i=0;i<nums.length;i++) {
+	       int q = nums[i];
+	       if (prev6==false) {
+		   if (q==6) {
+		       prev6=true;
+		   } else {
+		       sum+=q;
+		   }
+	       } else {
+		   if (q==7) {
+		       prev6=false;
+		   }
+	       }
+	   }
+	   return sum;
+       }
+
+
+    public boolean more14(int[] nums) {
+	int one=0;
+	int four=0;
+	for (int i=0;i<nums.length;i++) {
+	    if (nums[i]==1) {
+		one+=1;
+	    }
+	    if (nums[i]==4) {
+		four+=1;
+	    }
+	}
+	return one>four;
+    }
+
+    public int[] tenRun(int[] nums) {
+	int multiple=-1;
+	for (int i=0;i<nums.length;i++) {
+	    if (nums[i]%10==0) {
+		multiple=nums[i];
+	    } else if (multiple!=-1) {
+		nums[i]=multiple;
+	    }
+	}
+	return nums;
+    }
+
+    public boolean tripleUp(int[] nums) {
+	if (nums.length==0) {
+	    return false;
+	}
+	int previous=nums[0];
+	int count=1;
+	for (int i=1;i<nums.length;i++) {
+	    if (nums[i]-1==previous) {
+		count+=1;
+		if (count==3) return true;
+	    } else {
+		count=1;
+	    }
+	    previous=nums[i];
+	}
+	if (count==3) {
+	    return true;
+	}
+	return false;  
+    }
+    public boolean canBalance(int[] nums) {
+	int firstsum = 0;
+	int lastsum = 0;
+	for (int i =0; i < nums.length;i++){
+	    firstsum+=nums[i];
+	    for (int q=i+1;q<nums.length;q++){
+		lastsum+=nums[q];
+	    }
+	    if (firstsum==lastsum){
+		return true;
+	    }
+	    lastsum = 0;
+	}
+	return false;
+    }
+
+    public int[] seriesUp(int n) {
+	int[] newarray = new int[n*(n+1)/2];
+	int count = 0;
+	int num = 1;
+	for (int i=0; i<newarray.length; i++) {
+	    newarray[i]=num;
+	    num+=1;
+	    if (i==count){
+		count+=num;
+		num=1;
+	    }
+	}
+	return newarray;
+    }
+
+}
+
