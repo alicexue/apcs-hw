fb764653fd31707571489a936ba3a5a89e9e738a
Alice Xue <alicex123@gmail.com>
2014-11-20 21:14:22 -0500

    added vertical

--- 18-WordSearch/WordSearch.java
+++ 18-WordSearch/WordSearch.java
@@ -12,9 +12,9 @@ public class WordSearch{
 	    for (int j = 0; j < board[i].length; j++) {
 		board[i][j]='.';
 	    }
-	}
-	
+	}	
     }
+
     public WordSearch(){
 	this(20,30);
     }
@@ -30,29 +30,44 @@ public class WordSearch{
 	return s;
     }
     
-    public void addWordH(String w,int row, int col){
-	int r = row, c = col;
-
-	// if the row or col is out of bounds,
-	// will adjust them for the word to be at the last row or col
-	if (r>=20) {
-	    r = 19;
+    public int checkrow(int row) {
+	if (row>=20) {
+	    row = 19;
 	}
+	return row;
+    }
 
-	if (c>=30-w.length()) {
-	    c = 30-w.length();
+    public int checkcol(String w, int col) {
+	if (col>=30-w.length()) {
+	    col = 30-w.length();
 	}
+	return col;
+    }
 
-	// throws an error if the word cannot overlap 
-	//with the word already there
-	int d= c;
-
-	for (int i=0;i<w.length();i++) {
-	    if (board[r][d] != w.charAt(i) && board[r][d] != '.') {
-		throw new ArrayIndexOutOfBoundsException();
+    public void checkbounds(String w, int r, int c, int o) {
+	if (r==o) {
+	    for (int i=0;i<w.length();i++) {
+		if (board[r][c] != w.charAt(i) && board[r][c] != '.') {
+		    throw new ArrayIndexOutOfBoundsException();
+		}
+		r++;
 	    }
-	    d++;
+	} else {
+	    for (int i=0;i<w.length();i++) {
+		if (board[r][c] != w.charAt(i) && board[r][c] != '.') {
+		    throw new ArrayIndexOutOfBoundsException();
+		}
+		c++;
+	    }
 	}
+    }
+	
+
+    public void addWordH(String w,int row, int col){
+	int r = checkrow(row);
+	int c = checkcol(w,col);
+
+	checkbounds(w,r,c,c);
 
 	for (int i=0;i<w.length();i++) {
 	    board[r][c] = w.charAt(i);
@@ -61,48 +76,52 @@ public class WordSearch{
     }	   
 
     public void addWordHbw(String w,int row, int col){
-	int r = row, c = col;
+	int r = checkrow(row);
+	int c = checkcol(w,col);
+	
+	checkbounds(w,r,c,c);
 
-	// if row or col is out of bounds,
-	// will adjust them for the word to be at the last row or col
-	if (r>=20) {
-	    r = 19;
-	}
-
-	if (c>=30-w.length()) {
-	    c = 30-w.length();
-	}
-
-	// throws an error if the word cannot overlap 
-	//with the word already there
-	int d= c;
-
-	for (int i=w.length()-1;i>=0;i--) {
-	    if (board[r][d] != w.charAt(i) && board[r][d] != '.') {
-		throw new ArrayIndexOutOfBoundsException();
-	    }
-	    d--;
-	}
-
-	for (int i=w.length()-1;i>=0;i--) {
+	for (int i=0;i<w.length();i++) {
 	    board[r][c] = w.charAt(i);
 	    c--;
 	}
     }
-    
+
+    public void addWordV(String w, int row, int col) {
+	int r = checkrow(row);
+	int c = checkcol(w,col);
+	
+	checkbounds(w,r,c,r);
+
+	for (int i=0;i<w.length();i++) {
+	    board[r][c] = w.charAt(i);
+	    r++;
+	}
+    }
+
+    public void addWordVbw(String w, int row, int col) {
+	int r = checkrow(row);
+	int c = checkcol(w,col);
+
+	checkbounds(w,r,c,r);
+
+	for (int i=0;i<w.length();i++) {
+	    board[r][c] = w.charAt(i);
+	    r--;
+	}
+    }
+
+
     public static void main(String[] args) {
 	WordSearch w = new WordSearch();
-	System.out.println(w);
 	w.addWordH("hello",3,0);
-	System.out.println(w);
-	w.addWordH("look",3,3);
-	System.out.println(w);
+	w.addWordH("look",3,7);
 	w.addWordH("hello",100,500);
 	w.addWordH("hello",5,555);
-	System.out.println(w);
 	w.addWordHbw("hello",7,555);
+	w.addWordHbw("backwards",17,15);
+	w.addWordV("hello",7,14);
+	w.addWordVbw("hello",7,14);
 	System.out.println(w);
-	w.addWordHbw("boop",5,5);
-	System.out.println(w);
-    }
+	}
 }
