200f2c513ed5f932443957c7b06b4428074d1acf
Alice Xue <alicex123@gmail.com>
2014-12-03 21:06:52 -0500

    X

--- 18-WordSearch/WordSearch.java
+++ 18-WordSearch/WordSearch.java
@@ -7,18 +7,36 @@ import java.util.*;
 
 public class WordSearch{
 
-    private char[][] board;
-    private Random rnd = new Random();
+    private char[][] board, key;
+    private ArrayList<String> words;
+    private ArrayList<String> wordsInPuzzle;
+    private Random rnd;
     
+
     public WordSearch(int r, int c){
+	rnd = new Random();
+	words = new ArrayList<String>();
+
+	Scanner sc = null;
+
+	try {
+	    sc = new Scanner(new File("words.txt"));
+	} catch (Exception e) {
+	    System.out.println("Can't open wordlist");
+	    System.exit(0);
+	}
+
+	while (sc.hasNext()){
+	    words.add(sc.next());
+	}
 	board = new char[r][c];
 	for (int i = 0; i < board.length; i++) {
 	    for (int j = 0; j < board[i].length; j++) {
 		board[i][j]='.';
 	    }
-	}	
+	}
+	
     }
-
     public WordSearch(){
 	this(20,30);
     }
@@ -34,23 +52,30 @@ public class WordSearch{
 	return s;
     }
 
-    public boolean checkbounds(String w, int r, int c, int addrow, int addcol) {
-	boolean addword=true;
-	try {
-	    for (int i=0;i<w.length();i++) {
+    public boolean checkbounds(String w, int row, int col, int addrow, int addcol) {
+	int r = row, c = col;
+	for (int i=0;i<w.length();i++) {
+	    try {
 		if (board[r][c] != w.charAt(i) && board[r][c] != '.') {
-		    addword=false;
+		    return false;
 		}
-		r+=addrow;
-		c+=addcol;
+	    } catch (Exception e) {
+		return false;
 	    }
-	} catch (Exception e) {
-	    addword=false;
+	    r+=addrow;
+	    c+=addcol;
 	}
-	return addword;
+	r = row;
+	c = col;
+	for (int i=0;i<w.length();i++){
+	    board[r][c] = w.charAt(i);
+	    r+=addrow;
+	    c+=addcol;
+	}
+	return true;
     }
 
- 
+    /*
     public void addWordH(String w,int r, int c, boolean forward){
 	boolean addword=true;
 	if (forward) {
@@ -138,47 +163,55 @@ public class WordSearch{
 	    }
 	}
     }
-
+    */
+    /*
     public boolean addWord(String w) {
-	Random q = new Random();
-	int a = q.nextInt(4);
-	int r = q.nextInt(board.length);
-	int c = q.nextInt(board[0].length);
+	int a = rnd.nextInt(4);
+	int r = rnd.nextInt(board.length);
+	int c = rnd.nextInt(board[0].length);
 	boolean forward = true;
-	if (q.nextInt(2)==0) {
+	boolean canadd=false;
+	if (rnd.nextInt(2)==0) {
 	    forward = false;
 	}
 	if (a==0) {
 	    try {
 		addWordH(w,r,c,forward);
 	    } catch (Exception e) {
-		return false;
+		canadd=false;
 	    }
-	}
-	if (a==1) {
+	} else if (a==1) {
 	    try {
 		addWordV(w,r,c,forward);
 	    } catch (Exception e) {
-		return false;
+		canadd=false;
 	    }
-	}
-	if (a==2) {
+	} else if (a==2) {
 	    try {
 		addWordD(w,r,c,forward);
 	    } catch (Exception e) {
-		return false;
+		canadd=false;
 	    }
-	}
-	if (a==3) {
+	} else {
 	    try {
 		addWordDbw(w,r,c,forward);
 	    } catch (Exception e) {
-		return false;
+		canadd=false;
 	    }
 	}
-	return true;
+	return canadd;
     }
-
+*/
+    public boolean addWord(String w) {
+	int row = rnd.nextInt(board.length);
+	int col = rnd.nextInt(board[0].length);
+	int deltaRow = -1 + rnd.nextInt(3);
+	int deltaCol = -1 + rnd.nextInt(3);
+	if (deltaRow == deltaCol && deltaCol == 0)
+	    return false;
+	return checkbounds(w,row,col,deltaRow,deltaCol);
+    }
+    /*
     public void addLetters(){
 	char[] letters = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
 	for (int r=0;r<board.length;r++) {
@@ -189,10 +222,58 @@ public class WordSearch{
 	    }
 	}
     }
+    */
+    private void makeKey(){
+	key = new char[board.length][board[0].length];
+	for (int i = 0; i < board.length; i++) {
+	    for (int j = 0; j < board[0].length; j++) {
+		key[i][j] = board[i][j];
+	    }
+	}
+    }
+		
+    public void buildPuzzle(int numwords){
+	wordsInPuzzle = new ArrayList<String>();
+	while (numwords > 0){
+	    int wordIndex = rnd.nextInt(words.size());
+	    String word = words.get(wordIndex);
+	    if (addWord(word)) {
+		numwords--;
+		words.remove(wordIndex);
+		wordsInPuzzle.add(word);
+	    }
+	}
+	makeKey();
+	for (int i = 0; i < board.length; i++) {
+	    for (int j = 0; j < board[0].length; j++) {
+		if (board[i][j]=='.'){
+		    String letters = "abcdefghijklmnopqrstuvwxyz";
+		    char letter = letters.charAt(rnd.nextInt(letters.length()));
+		    board[i][j]=letter;					
+  		}
+	    }
+	}
+    }
 
+
+    public String getWIP() {
+	return wordsInPuzzle.toString();
+    }
+
+    public String getKey() {
+	String s = "";
+				
+	for (int i = 0; i < key.length; i++) {
+	    for (int j = 0; j < key[i].length; j++) {
+		s = s + key[i][j];
+	    }
+	    s = s + "\n";
+	}
+	return s;
+    }
     
     public static void main(String[] args) {
-	WordSearch w = new WordSearch();
+	WordSearch w = new WordSearch(10,40);
 	/*
 	w.addWordH("hello",3,0,true);
 	w.addWordH("look",3,7,true);
@@ -211,7 +292,7 @@ public class WordSearch{
 	w.addWord("turkeys");
 	w.addWord("potatoes");
 	*/
-
+	/*
 	try {
 	    Scanner sc = new Scanner (new File ("words.txt"));
 	    while (sc.hasNext()) {
@@ -221,11 +302,15 @@ public class WordSearch{
 	} catch (Exception e) {
 	    System.out.println("Can't open file");
 	}
-
+	*/
 
 		
-	w.addLetters();
-
+	//w.addLetters();
+	
 	System.out.println(w);
+	w.buildPuzzle(50);
+	System.out.println(w);
+	System.out.println(w.getWIP());
+	System.out.println(w.getKey());
     }
 }
