a96d6842165f4f3bc9ce142afeeed467c5451fd8
Alice Xue <alicex123@gmail.com>
2014-12-17 08:39:59 -0500

    x

--- classwork/Gui2.java
+++ classwork/Gui2.java
@@ -1,31 +1,84 @@
 import javax.swing.*;
 import java.awt.*;
-// java layout manager, java swing components
-public class Gui1 extends JFrame {
+import java.awt.event.*;
+
+public class Gui2 extends JFrame implements ActionListener {
 
     private Container pane;
-    private JButton b1,b2;
+    private JButton b1,b2,labeler;
     private JLabel l;
     private JTextArea text;
     private JPanel canvas;
+    private JCheckBox box;
+    private int x = 10;
+    private int y = 10;
+
     
-    public Gui1(){
+    public void actionPerformed(ActionEvent e) {
+	
+	if (e.getSource() == b1) {
+	    System.out.println("You clicked click me");
+	    System.out.println("By the way, the text box has: "+text.getText());
+	    System.out.println();
+	} else if (e.getSource() == b2) {
+	    System.out.println("Shutting down");
+	    System.exit(0);
+	} else if (e.getSource() == labeler) {
+	    labeler.setText(text.getText());
+	    
+	}
+	
+    }
+
+    private class Canvas extends JPanel {
+	public void paintComponent(Graphics g) {
+	    super.paintComponent(g);
+	    g.setColor(Color.red);
+	    g.fillOval(30,30,30,30);
+	}
+    }
+    
+    private class Key implements KeyListener {
+	public void keyPressed(KeyEvent e) {}
+	public void keyReleased(KeyEvent e) {}
+	public void keyTyped(KeyEvent e) {
+	    if (box.isSelected()) {
+		String s = text.getText();
+		s = s.toUpperCase();
+		text.setText(s);
+	    }
+	}
+    }    
+       
+    
+    public Gui2(){
+
 	setTitle("My First Gui");
-	setSize(600,400);
+	setSize(600,600);
 	setLocation(100,100);
 	setDefaultCloseOperation(EXIT_ON_CLOSE);
-	
+
 	pane = getContentPane();
 	//pane.setLayout(new GridLayout(3,3));
 	pane.setLayout(new FlowLayout());
 	l = new JLabel("The Label:");
 	pane.add(l);
-	
-	b1 = new JButton("button");
+						
+	b1 = new JButton("click me");
+	b1.addActionListener(this);
 	pane.add(b1);
 	b2 = new JButton("exit");
+	b2.addActionListener(this);
 	pane.add(b2);
-      	// text = new JTextArea("HELLO");
+
+	labeler = new JButton("labeler");
+	labeler.addActionListener(this);
+	pane.add(labeler);
+
+	box = new JCheckBox("Cap");
+	box.addActionListener(this);
+	pane.add(box);
+	
 	text = new JTextArea();
 	text.setColumns(40);
 	text.setRows(10);
@@ -36,27 +89,11 @@ public class Gui1 extends JFrame {
 	canvas.setPreferredSize(new Dimension(300,300));
 	canvas.setBorder(BorderFactory.createLineBorder(Color.blue,2));
 	pane.add(canvas);
-	
-	/* 
-	   add more buttons and add a new grid of 6 buttons to the bigger grid box
-	pane.add(new JButton("hello1"));
-	pane.add(new JButton("hello2"));
-	pane.add(new JButton("hello3"));
-
-	JPanel f2 = new JPanel();
-	pane.add(f2);
-	f2.setLayout(new GridLayout(3,3));
-	f2.add(new JButton("aaa"));
-	f2.add(new JButton("aaa"));
-	f2.add(new JButton("aaa"));
-	f2.add(new JButton("aaa"));
-	f2.add(new JButton("aaa"));
-	f2.add(new JButton("aaa"));
-	*/
+				
     }
-
+		
     public static void main(String[] args) {
-	Gui1 f = new Gui1();
+	Gui2 f = new Gui2();
 	f.setVisible(true);
     }
 }
